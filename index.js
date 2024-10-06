f3b835cd9fbeb5e1dda5157ce90f823bba4fa2a1
Akhil Tiwari <125031693+akhiltiwarietawah@users.noreply.github.com>
2024-10-07 04:53:06 +0530

    adding url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
     origin: [
-      
+      "https://device-trackingbyakhiltiwari.vercel.app"
       "https://device-tracking.vercel.app" // Your production URL
     ],
     methods: ["GET", "POST"],
